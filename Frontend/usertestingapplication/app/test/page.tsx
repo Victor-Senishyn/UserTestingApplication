4acf31a8c36e25c330a939f2867845d5ee02342b
Victor-Senishyn <senishin2004@gmail.com>
2024-03-11 10:41:18 +0200

    Updated frontend

--- Frontend/usertestingapplication/app/test/page.tsx
+++ Frontend/usertestingapplication/app/test/page.tsx
@@ -1,18 +1,29 @@
 'use client'
 import React, { useEffect, useState } from 'react'
+import { Tests } from '../components/Tests'
+import { Button } from 'antd'
+import { getAvailableTests, getCompletedTests } from '../services/tests'
+import { getQuestions } from '../services/questions'
 
 const Test = () => {
-	const [id, setId] = useState<string | null>(null)
+	
+	const [tests, setTests] = useState<Test[]>([])
 
 	useEffect(() => {
 		const urlParams = new URLSearchParams(window.location.search)
 		const idParam = urlParams.get('id')
-		if (idParam) {
-			setId(idParam)
+		const getQuest = async () => {
+			const tests = await getQuestions(Number(idParam))
+			setTests(tests)
 		}
+		getQuest()
 	}, [])
-	//getQuestions(id)
-	return <div className='card__buttons'>{id}</div>
+
+	return (
+		<div>
+			<Tests tests={tests} />
+		</div>
+	)
 }
 
 export default Test
